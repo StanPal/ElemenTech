88a0457fb9596c3840773a2e193a2b91040838c1
yangxinye980302 <59936968+yangxinye980302@users.noreply.github.com>
2020-08-17 18:53:40 -0700

    //guard,health and movement

---
+++ Assets/Script/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public static HealthBar Instance;
+    public UnityEngine.UI.Slider slider;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    private Rigidbody2D rb;
+    public float JumpSpeed = 400.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+        rb.AddForce(movement * speed);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0, JumpSpeed));
+        }
+    }
+}
