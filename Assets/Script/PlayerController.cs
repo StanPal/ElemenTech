ec8286efedb41f63afbedf2590a445d242de620d
yangxinye980302 <59936968+yangxinye980302@users.noreply.github.com>
2020-08-26 11:33:59 -0700

    //Dashmovement for hero

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private Rigidbody2D rb;
     public float JumpSpeed = 400.0f;
+    public float DashSpeed = 50.0f;
 
     void Start()
     {
@@ -18,14 +19,26 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        //float moveVertical = Input.GetAxis("Vertical");
-
+        float moveVertical = Input.GetAxis("Vertical");
+     
         Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+       
         rb.AddForce(movement * speed);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                movement = new Vector2(moveHorizontal + DashSpeed, 0.0f);
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(new Vector2(0, JumpSpeed));
         }
+        
+
     }
 }
