2dcfa9958d6539fde4a2bf3b861ff91f521bd562
yangxinye980302 <59936968+yangxinye980302@users.noreply.github.com>
2020-09-02 00:29:36 -0700

    //dash

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -11,99 +11,139 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float JumpSpeed;
     public float DashSpeed;
-    private float DashTime;
-    public float startDashTime;
-    private int direction;
+    //private float DashTime;
+    //public float startDashTime;
+    //private int direction;
+    public float DashDelay = 0.5f;
+    private float DashCoolDown;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        DashTime = startDashTime;
+        StartCoroutine("CheckDash");
+        //DashTime = startDashTime;
 
     }
 
     void Update()
     {
-        if (direction == 0)
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                direction = 1;
-            }
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                direction = 2;
-            }
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                direction = 3;
-            }
-            if (Input.GetKeyDown(KeyCode.Z))
-            {
-                direction = 4;
-            }
-            if (Input.GetKeyDown(KeyCode.C))
-            {
-                direction = 5;
-            }
-
-        }
-        else
-        {
-            if (DashTime <= 0)
-            {
-                direction = 0;
-                DashTime = startDashTime;
-                rb.velocity = Vector2.zero;
-            }
-            else
-            {
-                DashTime -= Time.deltaTime;
-
-                if (direction == 1)
-                {
-                    rb.velocity = Vector2.left * speed;
-                }
-                else if (direction == 2)
-                {
-                    rb.velocity = Vector2.right * speed;
-                }
-                else if (direction == 3)
-                {
-                    rb.velocity = Vector2.up * JumpSpeed;
-                }
-                else if (direction == 4)
-                {
-                    rb.velocity = Vector2.left * DashSpeed;
-                }
-                else if (direction == 5)
-                {
-                    rb.velocity = Vector2.right * DashSpeed;
-                }
-
-            }
-
+            rb.AddForce(new Vector2(0, JumpSpeed));
         }
 
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(new Vector2(-speed, 0));
+        }
 
-        //float moveHorizontal = Input.GetAxis("Horizontal");
-        ////float moveVertical = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(new Vector2(speed, 0));
+        }
 
-        //Vector2 movement = new Vector2(moveHorizontal, 0.0f);
-
-        //rb.AddForce(movement * speed);
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    rb.AddForce(new Vector2(0, JumpSpeed));
-        //}
-
-        //if (Input.GetKeyDown(KeyCode.J))
+        //if (Input.GetKeyDown(KeyCode.C))
         //{
         //    rb.AddForce(new Vector2(DashSpeed, 0));
         //}
 
-
-
-
+        //if (Input.GetKeyDown(KeyCode.Z))
+        //{
+        //    rb.AddForce(new Vector2(-DashSpeed, 0));
+        //}
     }
+
+    IEnumerator CheckDash()
+    {
+        while (true)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                DashCoolDown = Time.deltaTime;
+                while ((Time.deltaTime - DashCoolDown) <= DashDelay)
+                {
+                    if (Input.GetKeyDown(KeyCode.LeftArrow))
+                    {
+                        rb.AddForce(new Vector2(-DashSpeed, 0));
+                    }
+                    yield return null;
+                }
+            }
+           
+            yield return null;
+        }
+       
+    }
+        
+
+
+
+
+    
 }
+     //if (direction == 0)
+     //   {
+     //       if (Input.GetKey(KeyCode.LeftArrow))
+     //       {
+     //           direction = 1;
+     //       }
+     //       if (Input.GetKey(KeyCode.RightArrow))
+     //       {
+     //           direction = 2;
+     //       }
+     //       if (Input.GetKeyDown(KeyCode.Space))
+     //       {
+     //           direction = 3;
+     //       }
+     //       if (Input.GetKeyDown(KeyCode.Z))
+     //       {
+     //           direction = 4;
+     //       }
+     //       if (Input.GetKeyDown(KeyCode.C))
+     //       {
+     //           direction = 5;
+     //       }
+
+     //   }
+     //   else
+     //   {
+     //       if (DashTime <= 0)
+     //       {
+     //           direction = 0;
+     //           DashTime = startDashTime;
+     //           rb.velocity = Vector2.zero;
+     //       }
+     //       else
+     //       {
+     //           DashTime -= Time.deltaTime;
+
+     //           if (direction == 1)
+     //           {
+     //               rb.velocity = Vector2.left* speed * Time.deltaTime;
+     //           }
+     //           else if (direction == 2)
+     //           {
+     //               rb.velocity = Vector2.right* speed * Time.deltaTime;
+     //           }
+     //           else if (direction == 3)
+     //           {
+     //               rb.velocity = Vector2.up* JumpSpeed * Time.deltaTime;
+     //           }
+     //           else if (direction == 4)
+     //           {
+     //               rb.AddForce(new Vector2(-DashSpeed, 0));
+     //           }
+     //           else if (direction == 5)
+     //           {
+     //               rb.AddForce(new Vector2(DashSpeed, 0));
+     //           }
+
+     //       }
