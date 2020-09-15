f1b164cdb489d8149f637635c5df6cae6fc1a44b
Stan Palisoc <46430858+StanPal@users.noreply.github.com>
2020-09-14 18:51:23 -0700

    - Change Fire Shield to triangle instead of circle (fixes bug)

--- Assets/Script/Hero/Guard.cs
+++ Assets/Script/Hero/Guard.cs
@@ -30,7 +30,7 @@ public class Guard : MonoBehaviour
     private void GuardMove()
     {
         isGuarding = true;
-        SummonEarthGuard();
+        SummonGuard();
     }
 
     private void Update()
@@ -46,23 +46,8 @@ public class Guard : MonoBehaviour
             }
             else
             {
-                switch (mHero.GetElement)
-                {
-                    case Elements.ElementalAttribute.Fire:
-                mShield.transform.position = Vector3.MoveTowards(mShield.transform.position, gameObject.transform.position, 1.0f);
-                        break;
-                    case Elements.ElementalAttribute.Earth:
-                mShield.transform.position = Vector3.MoveTowards(mShield.transform.position, new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y), 1.0f);
-                        break;
-                    case Elements.ElementalAttribute.Water:
-                        break;
-                    case Elements.ElementalAttribute.Air:
-                        break;
-                    default:
-                        break;
-                }
+                mShield.transform.position = Vector3.MoveTowards(mShield.transform.position, new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y + 0.5f), 1.0f);
             }
-            
         }
         if(isGuarding && ComboSkillOn)
         {
@@ -81,39 +66,14 @@ public class Guard : MonoBehaviour
     }
 
 
-    private void SummonEarthGuard()
+    private void SummonGuard()
     {
         if (!mShiendCreated)
         {
-            switch (mHero.GetElement)
-            {
-                case Elements.ElementalAttribute.Fire:
-                    mShield = Instantiate(Shield, new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y), Quaternion.identity);
-                    break;
-                case Elements.ElementalAttribute.Earth:
-                    mShield = Instantiate(Shield, new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y + 0.5f), Quaternion.identity);
-                    break;
-                case Elements.ElementalAttribute.Water:
-                    break;
-                case Elements.ElementalAttribute.Air:
-                    break;
-                default:
-                    break;
-            }
+            mShield = Instantiate(Shield, new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y + 0.5f), Quaternion.identity);             
             
             mShield.transform.localScale = new Vector3(mShieldSize,mShieldSize,mShieldSize);
             mShiendCreated = true;
         }
-    }
-
-    private void SummonFireGuard()
-    {
-        if(!mShiendCreated)
-        {
-            
-            mShield.transform.localScale = new Vector3(mShieldSize, mShieldSize, mShieldSize);
-
-            mShiendCreated = true;
-        }
-    }
+    }    
 }
