be1b70294eff1c71b569d640bd4de398b6d262b5
yang <yang.steve@outlook.com>
2020-10-23 09:47:10 +0800

    disappear platform

--- Assets/DIsappearPlatform.cs
+++ Assets/DIsappearPlatform.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class DIsappearPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float timeToTogglePlatform = 2;
+    public float currentTime = 0;
+    public bool enabled = true;
     void Start()
     {
-        
+        enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentTime += Time.deltaTime;
+        if(currentTime >= timeToTogglePlatform)
+        {
+            currentTime = 0;
+            TogglePlatform();
+        }
+
+        void TogglePlatform()
+        {
+            enabled = !enabled;
+           
+            gameObject.SetActive(enabled);
+
+            foreach (Transform main in gameObject.transform)
+            {
+                main.gameObject.SetActive(enabled);
+            }
+
+            //foreach(Transform child in gameObject.transform)
+            //{
+            //    child.gameObject.SetActive(enabled);
+            //}
+        }
     }
 }
