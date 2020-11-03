4429e98cc01a3f043f2d813ac4d1e758e9fa1750
wallacegao <13730852881@163.com>
2020-11-03 09:10:47 +0900

    Fix trap and trap is work now

--- Assets/Script/LevelTrap/IceSpikeMovement.cs
+++ Assets/Script/LevelTrap/IceSpikeMovement.cs
@@ -3,20 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class IceSpikeMovement : MonoBehaviour
-{
-    Rigidbody2D rb;    
-
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+{   
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<HeroStats>())
         {
-            rb.isKinematic = false;
+            GetComponentInChildren<IceSpikeTrap>().activeSpike();
         }
     }
 }
